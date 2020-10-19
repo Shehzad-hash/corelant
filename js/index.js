e82210eceb18a2d529b63f9fd0212f62bbb64dac
Shehzad <shehzadkhan1564008@gmail.com>
2020-10-19 06:41:38 -0700

    corelant

---
+++ js/index.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $(document).ready(function(){
+        $(".owl-carousel").owlCarousel({
+            loop: true,
+            margin: 10,
+            dots: true,
+            responsiveClass:true,
+            responsive:{
+                0:{
+                    items:1,
+                },
+                576:{
+                    items:2,
+                },
+                768:{
+                    items:3,
+                }
+            }
+        });
+    });
+});
